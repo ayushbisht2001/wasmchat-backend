43e468a469a165b22ae317bea6fa6f771be432f1
Anirudh <purohitanirudh632@gmail.com>
2024-08-17 10:07:57 +0530

    database added

--- wasmchat/wasm/views.py
+++ wasmchat/wasm/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from .models import person_collection
+from django.http import  HttpResponse, JsonResponse
+# from django.core.serializers import serialize
+from bson.json_util import dumps
+import json
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("<h1>App is running</h1>")
+
+def add_person(request):
+    records = {
+        "first_name":"john",
+        "last_name": "smith"
+    }
+    person_collection.insert_one(records)
+    return HttpResponse("new person is added")
+
+
+def get_all_person(request):
+    persons = list(person_collection.find())
+    data = dumps(persons)
+    return HttpResponse(data,content_type = "application/json")
+
