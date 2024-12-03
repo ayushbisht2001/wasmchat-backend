7f1f79d380fa231f96b3f038ddd25a0a994ff1b1
ayush <ayush@switchon.io>
2024-12-03 21:30:50 +0530

    let's go

--- wasmchat/wasm/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-# Create your models here.
-# from mongoengine import Document, StringField, IntField
-
-# class YourModel(Document):
-#     name = StringField(max_length=200, required=True)
-#     age = IntField(min_value=0, required=True)
